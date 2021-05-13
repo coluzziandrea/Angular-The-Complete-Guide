ab9514a2d486a09ab7d8850ff8b06506e975f799
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-05-13 17:43:53 +0200

    template for recipe

--- shopping-market/src/app/recipe/recipe-list/recipe-list.component.ts
+++ shopping-market/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'a test recipe',
+      'recipe for testing',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw3DRo7PmfXvsFfituF9mKjD&ust=1621006242319000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPCA_Pv8xvACFQAAAAAdAAAAABAD'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
