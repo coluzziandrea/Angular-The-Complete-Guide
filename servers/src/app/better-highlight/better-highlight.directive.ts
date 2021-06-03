f2f48d339f8671c9834c19c9501608868f976998
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-03 15:06:26 +0200

    used hostListener to update style on event from directive

--- servers/src/app/better-highlight/better-highlight.directive.ts
+++ servers/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
@@ -6,12 +12,25 @@ import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  @HostListener('mouseenter')
+  mouseOver(eventData: Event) {
     this.renderer.setStyle(
       this.elementRef.nativeElement,
       'background-color',
       'blue'
     );
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'background-color',
+      'transparent'
+    );
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'inherit');
   }
 }
