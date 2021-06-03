46d471159c0d7838d0acf75d0697d9118618d7e9
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-03 11:27:16 +0200

    added header navigation to shopping market app

--- shopping-market/src/app/header/header.component.ts
+++ shopping-market/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   collapsed = true;
 
+  @Output()
+  featureSelected: EventEmitter<string> = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onMenuSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
