0a584e69cec742a1201c000c947277f732f40e15
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-03 16:56:26 +0200

    made dropdown close from click elsewhere

--- shopping-market/src/app/shared/dropdown.directive.ts
+++ shopping-market/src/app/shared/dropdown.directive.ts
@@ -11,9 +11,10 @@ import {
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
-
-  @HostListener('click')
-  onClickListener() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
