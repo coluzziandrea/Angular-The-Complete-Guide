aaf2be6073327c0df9a7b46f5a51cf6acf82cc7f
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-03 14:28:49 +0200

    added form to add ingredient to shopping list

--- shopping-market/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shopping-market/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+  styleUrls: ['./shopping-edit.component.scss'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false })
+  nameInput!: ElementRef;
 
-  constructor() { }
+  @ViewChild('amountInput', { static: false })
+  amountInput!: ElementRef;
 
-  ngOnInit(): void {
+  @Output()
+  itemAdded: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddIngredient() {
+    const nameVal = this.nameInput.nativeElement.value;
+    const amountVal = this.amountInput.nativeElement.value;
+    if (!amountVal || !nameVal) {
+      alert('Input Not Valid!');
+      return;
+    }
+    this.itemAdded.emit(new Ingredient(nameVal, amountVal));
   }
-
 }
--- shopping-market/src/app/shopping-list/shopping-list.component.ts
+++ shopping-market/src/app/shopping-list/shopping-list.component.ts
@@ -14,4 +14,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onItemAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
