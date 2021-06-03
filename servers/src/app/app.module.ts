cf3e4da04b586d2e65d4b107af95988b20536ea4
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-03 15:00:00 +0200

    used renderer to make a better directive

--- servers/src/app/app.module.ts
+++ servers/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BasicHighlightDirective } from './basic-highlight/basic-highlight.direc
     CockpitComponent,
     ServerElementComponent,
     BasicHighlightDirective,
+    BetterHighlightDirective,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
