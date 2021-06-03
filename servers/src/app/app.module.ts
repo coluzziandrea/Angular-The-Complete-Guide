b8951d1faa220375dfc1fa20070655b5b250169e
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-03 14:49:43 +0200

    used directive to style component

--- servers/src/app/app.module.ts
+++ servers/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, CockpitComponent, ServerElementComponent],
+  declarations: [
+    AppComponent,
+    CockpitComponent,
+    ServerElementComponent,
+    BasicHighlightDirective,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
