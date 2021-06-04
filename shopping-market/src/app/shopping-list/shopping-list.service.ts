c17ce5e3afa33de3d61151e009b6b06064e74b20
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-04 15:12:14 +0200

    added event to notify ingredients changed

--- shopping-market/src/app/shopping-list/shopping-list.service.ts
+++ shopping-market/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   ingredients: Ingredient[] = [
     new Ingredient('Apples', '5'),
     new Ingredient('Milk', '12 lts'),
@@ -12,5 +14,8 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+
+    // notify listeners that ingredients array is changed.
+    this.ingredientsChanged.emit(this.getIngredients());
   }
 }
