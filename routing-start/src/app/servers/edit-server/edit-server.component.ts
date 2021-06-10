1ee7e0fb3bb7e3265af9b2efaef8ee47c7311b64
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-06-10 14:16:08 +0200

    prevent leave the current path

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Observable } from "rxjs";
 
 import { ServersService } from "../servers.service";
+import { CanComponentDeactivate } from "./can-deactivate-guard.service";
 
 @Component({
   selector: "app-edit-server",
   templateUrl: "./edit-server.component.html",
   styleUrls: ["./edit-server.component.css"],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: { id: number; name: string; status: string };
   serverName = "";
   serverStatus = "";
   allowEdit = false;
+  changesSaved = false;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -29,6 +33,7 @@ export class EditServerComponent implements OnInit {
 
     const serverid = +this.route.snapshot.params["id"];
     this.server = this.serversService.getServer(serverid);
+
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
@@ -38,5 +43,22 @@ export class EditServerComponent implements OnInit {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(["../"], { relativeTo: this.route });
+  }
+
+  canDeactivate(): boolean | Observable<boolean> | Promise<boolean> {
+    if (!this.allowEdit) {
+      return true;
+    }
+    const serverChanged =
+      this.serverName !== this.server.name ||
+      this.serverStatus !== this.server.status;
+
+    if (serverChanged && !this.changesSaved) {
+      return confirm("Do You want to discard changes?");
+    } else {
+      return true;
+    }
   }
 }
